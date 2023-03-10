6b2db58da02ed3adf9321647cf8703c0c1fa72c1
dibbadan <daniele.dibattista@studenti.polito.it>
2023-03-10 13:16:38 +0100

    Matrix completed

--- matrix/src/main/kotlin/Matrix.kt
+++ matrix/src/main/kotlin/Matrix.kt
@@ -1,10 +1,11 @@
 class Matrix(private val matrixAsString: String) {
 
+    private val rows = matrixAsString.trim().split("\n").map { it.trim().split(" ").map { it.toInt() } }
     fun column(colNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
+        return rows.map { it[colNr - 1] }
     }
 
     fun row(rowNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
+        return rows[rowNr - 1]
     }
 }
